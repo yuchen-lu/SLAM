e00a754b177f68119ddca024a0d4db79099ba818
YUchen <yuchen.lu@mail.utoronto.ca>
2017-11-15 20:14:41 -0500

    opencv error

---
+++ ch8/useLk/build/CMakeFiles/uselk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uselk.dir/main.cpp.o"
+  "uselk.pdb"
+  "uselk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uselk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
