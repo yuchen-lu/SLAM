ac5bf56a7930aa4b23fd68dbd06a5f88d02f7872
YUchen <yuchen.lu@mail.utoronto.ca>
2017-12-08 20:44:46 -0500

    head files and cpp

---
+++ project/0.1/include/myslam/camera.h
@@ -0,0 +1,34 @@
+#ifndef CAMERA_H   // Add this to avoid repeat, needed for every 
+#define CAMERA_H
+#include "myslam/common_include.h" // store some common head file in commin_include.h to avoid large list of "include"
+
+
+namespace myslam   // give a namespace to avoid defining other functions, needed for every
+{
+  // Pinhole rgb-d camera model
+  
+  class camera
+  {
+    
+  public:
+    typedef std::shared_ptr<Camera> Ptr;   // define pointer as camera pointer type, call ---Camera::Ptr 
+    float fx_, fy_, cx_, cy_, depth_scale_: // camera K      // use Sophus::SE3 to rep pose
+    fx_ (fx), fy_(fy), cx_(cx), cy_(cy), depth_scale_(depth_scale_)
+  
+    {}
+    // coordinate transform : world, camera, pixel
+    Vector3d world2camera( const Vector3d& p_w, const SE3& T_c_w );
+    Vector3d camera2world ( const Vector3d& p_c, const SE3& T_c_w);
+    Vector2d camera2pixel( const Vector3d& p_c);
+    Vector3d pixel2camera( const Vector2d& p_p, double depth=1);
+    Vector3d pixel2world( const Vector2d& p_p, const SE3 T_c_w, double depth=1);
+    Vector2d world2pixel ( const Vector3d& p_w, const SE3& T_c_w);
+    
+    
+  };
+}
+#endif //CAMERA_H
+  
+  
+  
+}
---
+++ project/0.1/include/myslam/frame.h
@@ -0,0 +1,54 @@
+class Frame
+{
+public:
+	typedefstd::shared_ptr<Frame> Ptr;
+	unsigned long id_; // id of this frame
+	double time_stamp_; // when it is recorded
+	SE3 T_c_w_; // transofrom from world to camera-- pose
+	Camera::Ptr camera_; // pinhole rgb-d camera model
+	Mat color_, depth_; //color and depth image
+	
+public:		//data members
+	Frame();
+	Frame( long id, double time_stamp_ =0, SE3	 T_c_w_=SE3(), Camera::Ptr camera= nullptr, 
+	       Mat color =Mat(), Mat depth = Mat()  ) ;  // see how the initial value are defined by different variable names! (with/ without understroke)
+	-Frame(); //whats this?
+	
+	//factory function
+	static Frame::Ptr createFrame();
+	
+	//find the depth in depth map
+	double findDepth ( const cv::KeyPoint& kp);
+	
+	// get camera centre
+	Vector3d getCamCeneter() const;
+	
+	// check if a point is in this frame
+	
+	bool isInFrame( const Vector3d& pt_ world);
+	
+	
+};
+
+
+
+class MapPoint // MapPoint is landmark, estimate world pose, 
+//use feature points from prev frame to match landmarks to esti pose
+// so we need to store its descriptor
+// also record # of times a ppoint being Observed and being Matched
+{
+	typedef shared_ptr<MapPoint> Ptr;
+	unsigned long id_; //id
+	vector3d pos_; //world pose
+	Vector3d norm_; //normal of viewing direction?
+	Mat descriptor_; // descriptor for matching
+	int observed_times_;//.being ovserved by deature matching algo
+	int correct_times_; //being an inlinder in ppose estimation
+	
+	MapPoint();
+	MapPoint( long id, Vector3d posi	tion, Vector3d norm);
+	
+	//factoring function
+	static MapPoint::Ptr createMapPoint();
+	
+};
