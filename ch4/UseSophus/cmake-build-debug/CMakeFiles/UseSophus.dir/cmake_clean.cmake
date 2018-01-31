ba632c05a9d47602e7752d6ea9a9e46cc38aba54
yuchen-lu <yuchen.lu@mail.utoronto.ca>
2018-01-30 20:07:41 -0500

    more clear cmt

---
+++ ch4/UseSophus/cmake-build-debug/CMakeFiles/UseSophus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UseSophus.dir/UseSophus.cpp.o"
+  "UseSophus.pdb"
+  "UseSophus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UseSophus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
